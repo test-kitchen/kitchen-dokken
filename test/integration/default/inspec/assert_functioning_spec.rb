06ea18b99237ca7d0dfc8363d0e5e8ec2ffccb72
Sean OMeara <sean@sean.io>
2017-06-13 18:42:50 +0200

    s/amazonlinx/fedora/ in inspec

--- test/integration/default/inspec/assert_functioning_spec.rb
+++ test/integration/default/inspec/assert_functioning_spec.rb
@@ -1,15 +1,15 @@
 
 describe command('docker --host 127.0.0.1 ps') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/hello-amazonlinux$/) }
+  its(:stdout) { should match(/hello-fedora$/) }
 end
 
 describe command('docker --host 127.0.0.1 ps') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/hello-amazonlinux-data$/) }
+  its(:stdout) { should match(/hello-fedora-data$/) }
 end
 
 describe command('docker --host 127.0.0.1 ps') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should_not match(/helloagain-amazonlinux$/) }
+  its(:stdout) { should_not match(/helloagain-fedora$/) }
 end
