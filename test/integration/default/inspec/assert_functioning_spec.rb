b96a42d63c9490a27a15bd1ef53f81705a9d0af3
Sean OMeara <sean@sean.io>
2017-06-13 18:55:38 +0200

    fixing suite names in tests

--- test/integration/default/inspec/assert_functioning_spec.rb
+++ test/integration/default/inspec/assert_functioning_spec.rb
@@ -1,15 +1,15 @@
 
 describe command('docker --host 127.0.0.1 ps') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/hello-fedora$/) }
+  its(:stdout) { should match(/hello-fedora-latest$/) }
 end
 
 describe command('docker --host 127.0.0.1 ps') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/hello-fedora-data$/) }
+  its(:stdout) { should match(/hello-fedora-latest-data$/) }
 end
 
 describe command('docker --host 127.0.0.1 ps') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should_not match(/helloagain-fedora$/) }
+  its(:stdout) { should_not match(/helloagain-fedora-latest$/) }
 end
