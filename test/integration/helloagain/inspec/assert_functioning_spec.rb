b060d1024fc65cc093947c6b7334d092675fdf36
Dan Webb <dan.webb@damacus.io>
2022-04-05 11:33:42 +0100

    Chefstyle not cookstyle
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- test/integration/helloagain/inspec/assert_functioning_spec.rb
+++ test/integration/helloagain/inspec/assert_functioning_spec.rb
@@ -1,25 +1,25 @@
-control 'Verify Hostname' do
+control "Verify Hostname" do
   impact 0.7
-  title 'Hostname should be set'
-  desc 'Test to see if the hostname is set correctly via kitchen.yml'
+  title "Hostname should be set"
+  desc "Test to see if the hostname is set correctly via kitchen.yml"
 
   describe sys_info do
-    its('hostname') { should eq 'helloagain.computers.biz' }
+    its("hostname") { should eq "helloagain.computers.biz" }
   end
 end
 
-control 'Container group' do
+control "Container group" do
   impact 0.7
-  title 'HelloAgain should be able to access the hello container'
+  title "HelloAgain should be able to access the hello container"
 
-  describe host('hello.computers.biz:1234', port: 1234, protocol: 'tcp') do
+  describe host("hello.computers.biz:1234", port: 1234, protocol: "tcp") do
     it { should be_reachable }
   end
 end
 
-control 'Environment Variables' do
+control "Environment Variables" do
   impact 0.7
-  title 'Environment variables should be able to be passed through'
+  title "Environment variables should be able to be passed through"
 
   describe os_env("FOO") do
     its("content") { should eq "BAR" }
