ab78f9e6de4ce78eaae3f3383bbf5889429c9d81
Dan Webb <dan.webb@damacus.io>
2022-04-05 11:28:25 +0100

    Fix ordering of test suites.
    We can now bring up multiple containers and test them as per the how the
    tests were designed.
    
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- test/integration/helloagain/inspec/assert_functioning_spec.rb
+++ test/integration/helloagain/inspec/assert_functioning_spec.rb
@@ -1,9 +1,27 @@
+control 'Verify Hostname' do
+  impact 0.7
+  title 'Hostname should be set'
+  desc 'Test to see if the hostname is set correctly via kitchen.yml'
 
-describe command("curl -s hello.computers.biz:1234 | grep hello") do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/hello/) }
+  describe sys_info do
+    its('hostname') { should eq 'helloagain.computers.biz' }
+  end
 end
 
-describe os_env("FOO") do
-  its("content") { should eq "BAR" }
+control 'Container group' do
+  impact 0.7
+  title 'HelloAgain should be able to access the hello container'
+
+  describe host('hello.computers.biz:1234', port: 1234, protocol: 'tcp') do
+    it { should be_reachable }
+  end
+end
+
+control 'Environment Variables' do
+  impact 0.7
+  title 'Environment variables should be able to be passed through'
+
+  describe os_env("FOO") do
+    its("content") { should eq "BAR" }
+  end
 end
